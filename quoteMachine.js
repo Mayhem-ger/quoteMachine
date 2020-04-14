11fdfece5a04102d2470041d4adf5775b1ef8080
mayhem <you2.com>
2020-04-14 13:07:06 +0200

    Working on the twitter shit

--- quoteMachine.js
+++ quoteMachine.js
@@ -1,13 +1,10 @@
-const projectName = "random-quote-machine";
-localStorage.setItem('example_project', 'Randowm Quote Machine');
 let quotesData;
 
-function inIframe () { try { return window.self !== window.top; } catch (e) { return true; } }
-
 var currentQuote = '', currentAuthor = '';
+var url ="https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=testststst"
 
 function openURL(url){
-  window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
+  window.open(url);
 }
 
 function getQuotes() {
@@ -39,13 +36,8 @@ function getQuote() {
 
   $("#text").text(currentQuote);
   $("#author").text(currentAuthor);
+  $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
 
-
-  if(inIframe())
-  {
-    $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
-
-  }
 }
 
 $(document).ready(function() {
@@ -53,11 +45,10 @@ $(document).ready(function() {
     getQuote();
   });
 
-  $('#new-quote').on('click', getQuote);
+  $('#new-quote').on('click', getQuote)
 
-  $('.twitter-share-button').on('click', function() {
-    if(!inIframe()) {
-      openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
-    }
-  });
 });
+
+  $("#share").click(openURL(url));
+
+  //$('.twitter-share-button').on('click', openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=test' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor)));
