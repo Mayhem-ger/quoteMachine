6c98c9ea063abdd0497134865e3d843e76e4d4fd
Mayhem-ger <60428901+Mayhem-ger@users.noreply.github.com>
2020-04-14 09:28:18 +0200

    Only to do: Add Twitter Share option

--- quoteMachine.js
+++ quoteMachine.js
@@ -1,18 +1,17 @@
 $(document).ready(function(){
 
-$("#button").click(function ()
+$("#new-quote").click(function ()
   {
     $.ajax(settings).done(function (response) {
     	//feedback.push(response[0].quote);
-      console.log(response[0].quote, response[0].character)
-
-      $("#test").text(response[0].quote);
+      $("#text").text(response[0].quote);
+      let quoteText = response[0].quote;
       $("#author").text(response[0].character);
-      //feedback.push(response[0].author);
-
     });
 
   });
+
+$(".twitter-share-button").attr("href", "www.bild.de");
 
   var settings = {
   	"async": true,
